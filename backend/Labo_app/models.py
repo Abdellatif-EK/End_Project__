5fc42d9f1bf1b7f5827edceee1d45592e309cc53
Abdellatif EL KERBANI <abdellatif.elkerbani-etu@etu.univh2c.ma>
2024-06-04 14:08:15 +0100

    test commit

--- backend/Labo_app/models.py
+++ backend/Labo_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
-
+from django.db import models
 
 
 def upload_to(instance,filename):
@@ -115,7 +115,7 @@ class Matrice(models.Model):
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.ACTIF)
 
     def __str__(self):
-        return f'Matrice for {self.equipement.appareil} - {self.employe.username}'
+        return f'Matrice for  {self.equipement.appareil} - {self.employe.username}'
 
 
 # class Demande_intervention(models.Model):
